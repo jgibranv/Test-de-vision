680d0c58ac6e979b2d956c0069e1564411f9ba5c
jgibranv <57109955+jgibranv@users.noreply.github.com>
2021-03-19 21:22:43 -0700

    Add files via upload

---
+++ TestColor/TestColor/TestColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestColorApp.swift
+//  TestColor
+//
+//  Created by user189629 on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TestColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
